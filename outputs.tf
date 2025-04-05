d388025fb441152302fddb88343666eb5add90e3
Mahmoud Mohamed yousef <49796274+mahmoudaboseba@users.noreply.github.com>
2025-04-05 22:59:55 +0200

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,11 @@ output "instance_public_ip" {
   description = "Public IP address of the EC2 instance"
   value       = aws_instance.apache.public_ip
 }
+
+
+output "ssh_command" {
+  value = "ssh -i ec2_key.pem ec2-user@${aws_instance.apache.public_ip}"
+}
+  
+}
+
